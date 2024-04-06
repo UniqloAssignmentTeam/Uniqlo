0ba399409cf879bc915a5ea6f0de7f995aa470bf
Cheehan06 <cheehan1112@gmail.com>
2024-04-06 22:45:54 +0800

    ContactUs

--- Uniqlo/Pages/Cart.aspx.cs
+++ Uniqlo/Pages/Cart.aspx.cs
@@ -5,18 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Uniqlo.Pages
+namespace Uniqlo
 {
-    public partial class Cart : Page
+    public partial class ContactUs : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Calculate total items and total price
-            int totalItems = 9 + 5; // Total quantity of both items
-            decimal totalPrice = 809.91m + 499.95m; // Total price of both items
-            // Update total items and total price in the markup
-            totalItemsLabel.InnerText = totalItems.ToString();
-            totalPriceLabel.InnerText = totalPrice.ToString("F2"); // Display total price with two decimal places
+            // Code for page load event
         }
     }
 }
