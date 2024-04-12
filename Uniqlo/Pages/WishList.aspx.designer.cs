69d6b74dc21fb5ab79045dc4b1820b638c904449
IceBear1224 <93633714+IceBear1224@users.noreply.github.com>
2024-04-12 18:09:45 +0800

    wish list modal

--- Uniqlo/Pages/WishList.aspx.designer.cs
+++ Uniqlo/Pages/WishList.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Uniqlo.Pages
 
     public partial class WishList
     {
+
+        /// <summary>
+        /// RadioButtonList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
     }
 }
