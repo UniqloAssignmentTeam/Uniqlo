fb118bcbaf8fac510e8a539ea6fdd2e1516e6681
OhBengYee <ohby-pv21@student.tarc.edu.my>
2024-05-12 11:03:10 +0800

    push

--- Uniqlo/Pages/SignUp.aspx
+++ Uniqlo/Pages/SignUp.aspx
@@ -81,6 +81,12 @@
                     <div class="field button-field">
                         <asp:Button ID="btnSignUp" runat="server" Text="Sign Up" OnClick="btnSignUp_Click" />
                     </div>
+
+                    <!-- Message for image -->
+                    <div class="field button-field">
+                        <asp:Label ID="lblUploadMess" runat="server" Text="Label"></asp:Label>
+                        <asp:Label ID="lblReg" runat="server" Text="Label"></asp:Label>
+                    </div>
                 
                 
                 <!-- Redirect to Login -->
--- Uniqlo/Pages/SignUp.aspx.designer.cs
+++ Uniqlo/Pages/SignUp.aspx.designer.cs
@@ -229,5 +229,23 @@ namespace Uniqlo.Pages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSignUp;
+
+        /// <summary>
+        /// lblUploadMess control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUploadMess;
+
+        /// <summary>
+        /// lblReg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblReg;
     }
 }
